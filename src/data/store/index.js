3f9296ebf4ea93be6514a90215f4c634c476e083
I. Dan Calinescu <idancalinescu@gmail.com>
2017-05-26 23:17:06 +0300

    added redux tools

--- src/data/store/index.js
+++ src/data/store/index.js
@@ -6,7 +6,6 @@ import {
 import {
   composeWithDevTools
 } from 'remote-redux-devtools'
-import { reactotronRedux } from 'reactotron-redux'
 import promiseMiddleware  from 'redux-promise'
 import thunkMiddleware    from 'redux-thunk'
 import createLogger       from 'redux-logger'
@@ -16,17 +15,15 @@ import reducers           from '../reducers'
 const middleware = applyMiddleware(thunkMiddleware, promiseMiddleware, createLogger())
 
 // Setup the dev tools composer
-// const composeEnhancers = composeWithDevTools({
-//   name: 'Chunky',
-//   hostname: 'localhost',
-//   port: 8000,
-//   realtime: true
-// })
-// const enhancer = composeEnhancers(middleware)
-// Create the store from the reducers and enhancer
-// const store = (appReducers) => createStore(reducers(appReducers), enhancer)
+const composeEnhancers = composeWithDevTools({
+  name: 'Chunky',
+  hostname: 'localhost',
+  port: 8000,
+  realtime: true
+})
 
-const store = Reactotron.createStore(reducers(appReducers), compose(middleware))
+// Create the store from the reducers and middleware
+const store = (appReducers) => createStore(reducers(appReducers), composeEnhancers(middleware))
 
 // Export the store to be used by the entire app
 export default store
