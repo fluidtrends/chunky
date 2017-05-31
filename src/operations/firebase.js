1c159a65aa09b9e9922b6aac0cd14c1057344ccb
I. Dan Calinescu <idancalinescu@gmail.com>
2017-05-31 17:31:26 +0300

    added support for firebase logout

--- src/operations/firebase.js
+++ src/operations/firebase.js
@@ -4,7 +4,8 @@ import * as Utils from '../utils'
 import ChunkyError from '../core/Error'
 
 import {
-  retrieveAuthToken
+  retrieveAuthToken,
+  cacheAuthToken
 } from '../data/cache'
 
 export default class FirebaseOperation {
@@ -70,16 +71,14 @@ export default class FirebaseOperation {
     const email = this.props.username
     const password = this.props.password
 
-    return new Promise((resolve, reject) => {
-      firebase.auth().signInWithEmailAndPassword(email, password).
-              then((user) => {
-                  console.log("USER:", user)
-                  resolve({ user })
-                }).
-                catch((err) => {
-                  reject(Errors.INVALID_LOGIN_ERROR)
-                })
-    })
+    return firebase.auth().signInWithEmailAndPassword(email, password).
+            then((user) => {
+              // Let's inject the hash in the main token value
+              return cacheAuthToken({ user })
+            }).
+            catch((err) => {
+              throw Errors.INVALID_LOGIN_ERROR
+            })
   }
 
   facebookLogin() {
