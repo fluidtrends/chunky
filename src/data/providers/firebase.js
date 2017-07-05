1be3bff288cd3b92f0b3323ab4f2a4c5d5fa1252
I. Dan Calinescu <idancalinescu@gmail.com>
2017-07-05 17:49:10 +0300

    added firebase update support

--- src/data/providers/firebase.js
+++ src/data/providers/firebase.js
@@ -48,6 +48,28 @@ export default class FirebaseDataProvider extends DataProvider  {
           })
   }
 
+  update({ nodes, options, props }) {
+     // Let's see what kind of a resource we want to retrieve
+    const resource = nodes[0]
+
+    if (!resource) {
+      // We require a resource to be defined
+      return Promise.reject(Errors.UNDEFINED_OPERATION())
+    } 
+
+    // Let's see if we have a field we requested
+    const key = (nodes.length > 1 ? nodes[1] : undefined)
+    const keyValue = (key ? props[key] : undefined)
+    
+    if (!key || !keyValue || !props || !props.updates) {
+      // We're looking for a specific field, but no value was given
+      return Promise.reject(Errors.UNDEFINED_OPERATION())
+    }
+
+    // Update the specific fields
+    return firebase.database().ref(`${resource}/${keyValue}`).update(props.updates)
+  }
+
   retrieve({ nodes, options, props }) {
      // Let's see what kind of a resource we want to retrieve
     const resource = nodes[0]
@@ -57,8 +79,26 @@ export default class FirebaseDataProvider extends DataProvider  {
       return Promise.reject(Errors.UNDEFINED_OPERATION())
     } 
 
+    // Let's see if we have a field we requested
+    const field = (nodes.length > 1 ? nodes[1] : undefined)
+    const fieldValue = (field ? props[field] : undefined)
+    
     return new Promise((resolve, reject) => {
       const ref = firebase.database().ref(resource)
+
+      if (field) {
+        if (!fieldValue) {
+          // We're looking for a specific field, but no value was given
+          return Promise.reject(Errors.UNDEFINED_OPERATION())
+        }
+
+        ref.orderByChild(field).equalTo(fieldValue).on("child_added", function(snapshot) {
+          // Fetch the resource with the specified field
+          resolve(snapshot.val())
+        })
+        return
+
+      }
 
       if (options.latest) {
         ref.orderByChild("endDate").limitToLast(1).on("child_added", function(snapshot) {
