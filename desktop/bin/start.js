613c624a1cd8cd72f6fa09fed30fd369ee35b993
I. Dan Calinescu <idancalinescu@gmail.com>
2018-03-23 11:53:58 +0200

    updated the desktop skeleton

--- desktop/bin/start.js
+++ desktop/bin/start.js
@@ -3,14 +3,14 @@
 let path = require('path')
 let fs = require('fs-extra')
 let forgeStart = require('electron-forge/dist/api/start').default
-let forgeImport = require('electron-forge/dist/api/import').default
 
 process.env.CPPFLAGS = '-I/usr/local/opt/openssl/include'
 process.env.LDFLAGS = '-L/usr/local/opt/openssl/lib'
 
 function start (options) {
   const dir = path.resolve(options.dir)
-  const appPath = path.join('node_modules', 'react-electron-chunky', 'app')
+  // const appPath = path.join('node_modules', 'react-electron-chunky', 'app')
+  const appPath = path.join('desktop', 'app')
 
   return forgeStart({
     dir,
