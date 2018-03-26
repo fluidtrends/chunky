7e0dce9ffbab71c746f831d51df4ed82823a7ee3
AndyComan <andy@evanghelic.ro>
2018-03-26 16:29:51 +0300

    merge master

--- cli/executors/start/index.js
+++ cli/executors/start/index.js
@@ -1,28 +1,32 @@
 const coreutils = require('coreutils')
-const web = require("./web")
-const mobile = require("./mobile")
+const web = require('./web')
+const mobile = require('./mobile')
+const desktop = require('./desktop')
 
-function parseCommand(command) {
-    if (command.platforms.length == 0) {
-        command.platforms = ["mobile", "web"]
+function parseCommand (command) {
+  if (command.platforms.length === 0) {
+    command.platforms = ['mobile', 'web', 'desktop']
+  }
+
+  command.platforms.forEach(platform => {
+    switch (platform) {
+      case 'mobile':
+        mobile(command.mobilePackagerPort)
+        break
+      case 'web':
+        web(command.webPackagerPort)
+        break
+      case 'desktop':
+        desktop(command.desktopPackagerPort)
+        break
     }
-
-    command.platforms.forEach(platform => {
-        switch(platform) {
-            case 'mobile':
-                mobile(command.mobilePackagerPort)
-            break;
-            case 'web':
-                web(command.webPackagerPort)
-            break;
-        }
-    })
+  })
 }
 
-module.exports = function(command) {
-    try {
-        parseCommand(command)
-    } catch (error) {
-        coreutils.logger.error(error)
-    }
+module.exports = function (command) {
+  try {
+    parseCommand(command)
+  } catch (error) {
+    coreutils.logger.error(error)
+  }
 }
--- dev/index.js
+++ dev/index.js
@@ -3,7 +3,7 @@ const fs = require('fs-extra')
 const path = require('path')
 
 const watcher = chokidar.watch('.', {
-  ignored: /node_modules|bin|coverage|dev|.git/,
+  ignored: /node_modules|coverage|cli|dev|.git|.DS_Store/,
   persistent: true
 })
 
@@ -14,30 +14,60 @@ if (!dest || !fs.existsSync(path.resolve(dest))) {
   process.exit(0)
 }
 
-function update (src, remove) {
+function findTarget (src) {
   var paths = src.split('/')
   const type = paths.shift()
   const original = paths.join('/')
-  var target = path.resolve(dest)
 
+  var target = path.resolve(dest)
   switch (type) {
+    case 'desktop':
+      target = path.join(target, 'node_modules', 'react-electron-chunky', original)
+      break
     case 'web':
       target = path.join(target, 'node_modules', 'react-dom-chunky', original)
       break
     case 'mobile':
       target = path.join(target, 'node_modules', 'react-native-chunky', original)
       break
-    case 'cli':
-      console.log('Skipping', original)
-      return
     default:
       target = path.join(target, 'node_modules', 'react-chunky', src)
+  }
+
+  return target
+}
+
+function add (src) {
+  const target = findTarget(src)
+
+  if (!target) {
+    return
+  }
+
+  if (fs.existsSync(target)) {
+    return
+  }
+
+  fs.copySync(src, target)
+  console.log('Added', target)
+}
+
+function update (src, remove) {
+  const target = findTarget(src)
+
+  if (!target) {
+    return
   }
 
   remove ? fs.removeSync(target) : fs.copySync(src, target)
   console.log(remove ? 'Removed' : 'Moved', src, remove ? 'from' : 'to', target)
 }
 
+watcher.on('all', (type, src) => console.log(type, src))
+
 watcher
+  .on('add', src => add(src))
+  .on('addDir', src => add(src))
   .on('change', src => update(src))
   .on('unlink', src => update(src, true))
+  .on('unlinkDir', src => update(src, true))
