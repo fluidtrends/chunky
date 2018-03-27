54d87e4e559fd764f129a84b5ef61770c93f298a
I. Dan Calinescu <idancalinescu@gmail.com>
2018-03-27 19:11:09 +0300

    added web to the store template

--- templates/company/chunks/index.js
+++
@@ -1,5 +0,0 @@
-// AUTO-GENERATED FILE. PLEASE DO NOT MODIFY. CHUNKY WILL CRY.
-
-export { default as docs } from './docs'
-export { default as intro } from './intro'
-export { default as posts } from './posts'
--- templates/product/chunks/index.web.js
+++
@@ -1,5 +0,0 @@
-// AUTO-GENERATED FILE. PLEASE DO NOT MODIFY. CHUNKY WILL CRY.
-
-export { default as docs } from './docs/index.web'
-export { default as intro } from './intro/index.web'
-export { default as posts } from './posts/index.web'
