48c2fdae67cc7f991e504ccb4feb050994cb8a52
I. Dan Calinescu <idancalinescu@gmail.com>
2018-03-28 21:06:29 +0300

    added post installer for desktop

--- desktop/bin/install.js
+++ desktop/bin/install.js
@@ -3,4 +3,7 @@
 let path = require('path')
 let fs = require('fs-extra')
 
-console.log(__dirname, process.cwd())
+const src = path.join(process.cwd(), 'node_modules', 'electron-prebuilt-compile')
+const target = path.join(process.cwd(), 'react-electron-chunky', 'node_modules', 'electron-prebuilt-compile')
+
+fs.copySync(src, target)
