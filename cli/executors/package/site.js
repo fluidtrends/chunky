a5a983119343549d63be7dda15bd745e5cf6f6ca
I. Dan Calinescu <idancalinescu@gmail.com>
2018-03-31 07:14:42 +0300

    added desktop build script

--- cli/executors/package/site.js
+++
@@ -1,5 +0,0 @@
-const coreutils = require('coreutils')
-
-module.exports = function(optimize) {
-    coreutils.logger.info(`Packaging the Static Site ...`)
-}
---
+++ desktop/bin/build.js
@@ -0,0 +1,26 @@
+'use strict'
+
+let path = require('path')
+let fs = require('fs-extra')
+let forgePackage = require('electron-forge/dist/api/package').default
+
+process.env.CPPFLAGS = '-I/usr/local/opt/openssl/include'
+process.env.LDFLAGS = '-L/usr/local/opt/openssl/lib'
+
+function build (options) {
+  const dir = path.resolve('.')
+  const platform = process.platform
+  const arch = process.arch
+  const outDir = path.resolve(options.dir, 'desktop', 'build')
+
+  if (fs.existsSync(outDir)) { fs.removeSync(outDir) }
+
+  return forgePackage({
+    dir,
+    platform,
+    arch,
+    outDir
+  })
+}
+
+module.exports = build
