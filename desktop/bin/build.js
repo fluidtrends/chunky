4914ee6953c072db32c1d0dcde80cb430beace8d
I. Dan Calinescu <idancalinescu@gmail.com>
2018-04-17 18:27:25 +0300

    added intelligent cross platform desktop builder

--- desktop/bin/build.js
+++ desktop/bin/build.js
@@ -2,26 +2,34 @@
 
 let path = require('path')
 let fs = require('fs-extra')
-let forgePackage = require('electron-forge/dist/api/package').default
+let forgePackage = require('electron-forge/dist/api/make').default
 
 process.env.CPPFLAGS = '-I/usr/local/opt/openssl/include'
 process.env.LDFLAGS = '-L/usr/local/opt/openssl/lib'
 
-function build (options) {
-  const dir = path.resolve(options.dir)
-  const platform = process.platform
-  const arch = process.arch
-  const outDir = path.resolve(options.dir, 'desktop', 'build')
-
-  if (fs.existsSync(outDir)) { fs.removeSync(outDir) }
+function buildForPlatform (dir, outDir, platform) {
+  const parts = (platform ? platform.split('/') : [process.platform, process.arch])
 
   return forgePackage({
     dir,
     interactive: true,
-    platform,
-    arch,
+    platform: parts[0],
+    arch: parts[1],
     outDir
   })
+}
+
+function build (options) {
+  const dir = path.resolve(options.dir)
+  const outDir = path.resolve(options.dir, 'desktop', 'build')
+
+  if (fs.existsSync(outDir)) { fs.removeSync(outDir) }
+
+  if (options.config && options.config.build && options.config.build.platforms) {
+    return Promise.all(options.config.build.platforms.map(p => buildForPlatform(dir, outDir, p)))
+  }
+
+  return buildForPlatform(dir, outDir)
 }
 
 module.exports = build
