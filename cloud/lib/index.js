841f9e98c8d6a575750d131ce83565b6fbc09196
I. Dan Calinescu <idancalinescu@gmail.com>
2018-04-25 15:09:11 +0300

    added cloud emailer support

--- cloud/lib/index.js
+++ cloud/lib/index.js
@@ -4,10 +4,12 @@ var loader = require('./loader');
 var handler = require('./handler');
 var firebase = require('./firebase');
 var aws = require('./aws');
+var emailer = require('./emailer');
 
 module.exports = {
   loader: loader,
   handler: handler,
   firebase: firebase,
+  emailer: emailer,
   aws: aws
 };
--- cloud/src/index.js
+++ cloud/src/index.js
@@ -2,10 +2,12 @@ const loader = require('./loader')
 const handler = require('./handler')
 const firebase = require('./firebase')
 const aws = require('./aws')
+const emailer = require('./emailer')
 
 module.exports = {
   loader,
   handler,
   firebase,
+  emailer,
   aws
 }
