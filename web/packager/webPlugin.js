2474c239f1a561f8eeb3fe0c9f22a3b4950b360c
I. Dan Calinescu <idancalinescu@gmail.com>
2018-07-06 08:17:16 +0300

    added web packager support for ipc

--- web/packager/webPlugin.js
+++ web/packager/webPlugin.js
@@ -4,6 +4,7 @@ let chalk = require('chalk')
 let path = require('path')
 let Ora = require('ora')
 let ejs = require('ejs')
+const ipc = require('node-ipc')
 
 class Plugin {
 
@@ -11,6 +12,7 @@ class Plugin {
     this._context = context
     this._startTime = new Date().getTime()
     this._spinner = new Ora()
+    this._ipcConnected = false
   }
 
   get context () {
@@ -25,6 +27,10 @@ class Plugin {
     return this._startTime
   }
 
+  get ipcConnected () {
+    return this._ipcConnected
+  }
+
   log (message) {
     console.log(`${chalk.bold('[Chunky]')} ${message}`)
   }
@@ -32,11 +38,38 @@ class Plugin {
   onStart () {
     this._startTime = new Date().getTime()
     this.log(chalk.green('Getting ready to start packing'))
+    this.connectToStudio()
+    this.sendStudioEvent({ type: 'start_packing', time: this._startTime })
+  }
+
+  connectToStudio () {
+    if (this.ipcConnected) {
+      return
+    }
+
+    const name = 'chunkyweb'
+    ipc.config.id = 'name'
+    ipc.config.retry = 1500
+    ipc.config.silent = true
+
+    ipc.connectTo('carmelstudio', () => {
+      ipc.of.carmelstudio.on('connect', () => {
+        console.log(`[${name}] connected to carmelstudio`)
+      })
+    })
+  }
+
+  sendStudioEvent (event) {
+    if (!this.ipcConnected) {
+      return
+    }
+    ipc.of.carmelstudio.emit('event', event)
   }
 
   onDone (done) {
     const time = this.endTime(this.startTime)
     this.log(chalk.green('✔ Finished packing in ') + chalk.bold(time))
+    this.sendStudioEvent({ type: 'finished_packing', time })
     done && done()
   }
 
@@ -51,6 +84,7 @@ class Plugin {
 
     this.log(chalk.red('✘ ') + chalk.gray(module.resource))
     this.log(chalk.red(module.error))
+    this.sendStudioEvent({ type: 'module_error', module: module.resource, error: module.error })
   }
 
   onModuleSuccess (module) {
@@ -66,6 +100,7 @@ class Plugin {
     }
 
     this.log(chalk.green('✔ ') + chalk.gray(module.resource))
+    this.sendStudioEvent({ type: 'module_success', module: module.resource })
   }
 
   endTime (startTime) {
