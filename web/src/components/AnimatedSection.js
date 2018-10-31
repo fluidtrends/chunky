0d938b7aa57ca39213759824bf249f3c591a338d
Robert Pop <poprobertdaniel22@gmail.com>
2018-10-31 16:43:55 +0200

    cleanup

--- web/src/components/AnimatedSection.js
+++ web/src/components/AnimatedSection.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { AnimatedValue, Spring, animated, interpolate } from 'react-spring'
+import { Spring, animated, interpolate } from 'react-spring'
 
 const animationChecker = ['opacity', 'slideFromLeft', 'slideFromRight']
 /**
