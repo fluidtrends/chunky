7689b4c3b6e01e5b430a3d42c34a33d2765df202
Robert Pop <poprobertdaniel22@gmail.com>
2018-11-23 08:24:43 +0200

    summary without img option

--- web/src/components/Summary.js
+++ web/src/components/Summary.js
@@ -4,46 +4,68 @@ import Text from './Text'
 import { renderResponsive } from '../utils/responsive'
 
 export default class Summary extends Component {
-
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = { ...this.state }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     super.componentDidMount()
   }
 
-  renderText () {
-    return renderResponsive('text',
-      <Text source={this.props.text} style={{
-        width: `90vw`,
-        padding: '10px',
-        paddingBottom: '60px'
-      }} />,
-      <Text source={this.props.text} style={{
-        width: `70vw`,
-        paddingBottom: '60px'
-      }} />)
+  renderText() {
+    return renderResponsive(
+      'text',
+      <Text
+        source={this.props.text}
+        style={{
+          width: `90vw`,
+          padding: '10px',
+          paddingBottom: '60px'
+        }}
+      />,
+      <Text
+        source={this.props.text}
+        style={{
+          width: `70vw`,
+          paddingBottom: '60px'
+        }}
+      />
+    )
   }
 
-  renderComponent () {
-    return (<div style={{
-      color: this.props.textColor,
-      position: 'relative',
-      padding: '60px 0',
-      display: 'flex',
-      padding: '40px 0',
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center' }}>
-      <img src={`/assets/${this.props.image}`} style={{
-        width: '200px',
-        marginTop: '20px',
-        marginBottom: '-20px'
-      }} />
-      { this.renderText() }
-    </div>)
+  renderImg() {
+    if (!this.props.image) return null
+    return (
+      <img
+        src={`/assets/${this.props.image}`}
+        style={{
+          width: '200px',
+          marginTop: '20px',
+          marginBottom: '-20px'
+        }}
+      />
+    )
+  }
+
+  renderComponent() {
+    return (
+      <div
+        style={{
+          color: this.props.textColor,
+          position: 'relative',
+          padding: '60px 0',
+          display: 'flex',
+          padding: '40px 0',
+          flex: 1,
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        {this.renderImg()}
+        {this.renderText()}
+      </div>
+    )
   }
 }
