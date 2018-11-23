542c69e1a156d2e18ed6c4bcbfda87005ecfbf51
Robert Pop <poprobertdaniel22@gmail.com>
2018-11-23 14:41:52 +0200

    nav improvements

--- web/src/components/Feature.js
+++ web/src/components/Feature.js
@@ -6,189 +6,244 @@ import { renderResponsive } from '../utils/responsive'
 import { isAnyPartOfElementInViewport } from '../utils/isElementVisible'
 import { Button } from '@rmwc/button'
 
-
 export default class Feature extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = { ...this.state, startAnimation: false }
     this.handleScrollToElement = this.handleScrollToElement.bind(this)
   }
 
-  componentDidMount () {
+  componentDidMount() {
     super.componentDidMount()
-    window.addEventListener('scroll', this.handleScrollToElement);
+    window.addEventListener('scroll', this.handleScrollToElement)
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScrollToElement);
+    window.removeEventListener('scroll', this.handleScrollToElement)
   }
 
   handleScrollToElement() {
-    if (isAnyPartOfElementInViewport(this.blockRef) && !this.state.startAnimation) {
-      this.setState({startAnimation: true})
+    if (
+      isAnyPartOfElementInViewport(this.blockRef) &&
+      !this.state.startAnimation
+    ) {
+      this.setState({ startAnimation: true })
       window.removeEventListener('scroll', this.handleScrollToElement)
     }
   }
 
-  renderContent (compact) {
-    const animationType = this.props.reversed ? 'slideFromLeft' : 'slideFromRight'
+  renderContent(compact) {
+    const animationType = this.props.reversed
+      ? 'slideFromLeft'
+      : 'slideFromRight'
 
     if (this.props.animation) {
-      return <AnimatedSection 
-        animationType={window.innerWidth > 1224 ? animationType : 'slideFromLeft'}
-        startAnimation={this.state.startAnimation}
-      >
-        <div 
+      return (
+        <AnimatedSection
+          animationType={
+            window.innerWidth > 1224 ? animationType : 'slideFromLeft'
+          }
+          startAnimation={this.state.startAnimation}
+        >
+          <div
             style={{
+              display: 'flex',
+              flex: 1,
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingBottom: `${compact ? 100 : 0}px`
+            }}
+          >
+            {this.text()}
+            {this.button()}
+          </div>
+        </AnimatedSection>
+      )
+    } else {
+      return (
+        <div
+          style={{
             display: 'flex',
             flex: 1,
             flexDirection: 'column',
             alignItems: 'center',
             justifyContent: 'center',
             paddingBottom: `${compact ? 100 : 0}px`
-          }}>
-          
-            { this.text()}
-            { this.button()}
+          }}
+        >
+          {this.text()}
+          {this.button()}
         </div>
-      </AnimatedSection>
-    } else {
-      return <div 
-        style={{
-        display: 'flex',
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingBottom: `${compact ? 100 : 0}px`
-      }}>
-      
-        { this.text()}
-        { this.button()}
-      </div>
+      )
     }
   }
 
-  text () {
-    return renderResponsive('text',
-      <Text source={this.props.text} style={{
-        width: `90vw`,
-        marginBottom: '20px',
-        color: this.props.textColor
-      }} />,
-      <Text source={this.props.text} style={{
-        width: `40vw`,
-        color: this.props.textColor
-      }} />)
+  text() {
+    return renderResponsive(
+      'text',
+      <Text
+        source={this.props.text}
+        style={{
+          width: `90vw`,
+          marginBottom: '20px',
+          color: this.props.textColor
+        }}
+      />,
+      <Text
+        source={this.props.text}
+        style={{
+          width: `40vw`,
+          color: this.props.textColor
+        }}
+      />
+    )
   }
 
-  button () {
+  button() {
     if (!this.props.actionTitle) return null
-    return <Button style={{
-      marginBottom: '30px'
-    }}
-      onClick={this.triggerEvent()}
-      raised
-      theme='secondary-bg text-primary-on-secondary'>
-      {this.props.actionTitle}
-    </Button>
+    return (
+      <Button
+        style={{
+          marginBottom: '30px'
+        }}
+        onClick={this.triggerEvent()}
+        raised
+        theme="secondary-bg text-primary-on-secondary"
+      >
+        {this.props.actionTitle}
+      </Button>
+    )
   }
 
-  image () {
-    const animationType = this.props.reversed ? 'slideFromRight' : 'slideFromLeft'
-    const boxShadow = this.props.noBoxShadow ? '' : '0 5px 20px 0 rgba(0,0,0,.15)'
+  image() {
+    const animationType = this.props.reversed
+      ? 'slideFromRight'
+      : 'slideFromLeft'
+    const boxShadow = this.props.noBoxShadow
+      ? ''
+      : '0 5px 20px 0 rgba(0,0,0,.15)'
 
     if (this.props.animation) {
-      return <AnimatedSection 
-        animationType={window.innerWidth > 1224 ? animationType : 'slideFromLeft'}
-        startAnimation={this.state.startAnimation}
-      >
-        {renderResponsive('image', <img src={`/assets/${this.props.image}`} style={{
-          width: '90vw',
-          marginTop: '60px',
-          boxShadow,
-          marginBottom: '10px'
-        }} />,
-        <img src={`/assets/${this.props.image}`} style={{
-          width: '40vw',
-          marginTop: '60px',
-          boxShadow,
-          marginBottom: '60px'
-        }} />)}
-      </AnimatedSection>
+      return (
+        <AnimatedSection
+          animationType={
+            window.innerWidth > 1224 ? animationType : 'slideFromLeft'
+          }
+          startAnimation={this.state.startAnimation}
+        >
+          {renderResponsive(
+            'image',
+            <img
+              src={`/assets/${this.props.image}`}
+              style={{
+                width: '90vw',
+                marginTop: '60px',
+                boxShadow,
+                marginBottom: '10px'
+              }}
+            />,
+            <img
+              src={`/assets/${this.props.image}`}
+              style={{
+                width: '40vw',
+                marginTop: '60px',
+                boxShadow,
+                marginBottom: '60px'
+              }}
+            />
+          )}
+        </AnimatedSection>
+      )
     } else {
-      return renderResponsive('image', <img src={`/assets/${this.props.image}`} style={{
-        width: '90vw',
-        marginTop: '60px',
-        boxShadow,
-        marginBottom: '10px'
-      }} />,
-      <img src={`/assets/${this.props.image}`} style={{
-        width: '40vw',
-        marginTop: '60px',
-        boxShadow,
-        marginBottom: '60px'
-      }} />)
+      return renderResponsive(
+        'image',
+        <img
+          src={`/assets/${this.props.image}`}
+          style={{
+            width: '90vw',
+            marginTop: '60px',
+            boxShadow,
+            marginBottom: '10px'
+          }}
+        />,
+        <img
+          src={`/assets/${this.props.image}`}
+          style={{
+            width: '40vw',
+            marginTop: '60px',
+            boxShadow,
+            marginBottom: '60px'
+          }}
+        />
+      )
     }
   }
 
-  renderBlock (block, index) {
-    return <div
-      key={`block${index}`}
-      ref={ref => this.blockRef = ref}
-      style={{
-        display: 'flex',
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}>
-      { block }
-    </div>
+  renderBlock(block, index) {
+    return (
+      <div
+        key={`block${index}`}
+        ref={ref => (this.blockRef = ref)}
+        style={{
+          display: 'flex',
+          flex: 1,
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        {block}
+      </div>
+    )
   }
 
-  renderBlocks (blocks, compact) {   
+  renderBlocks(blocks, compact) {
     var index = 0
 
-    return <div 
-      style={{
-        color: '#607D8B',
-        position: 'relative',
-        display: 'flex',
-        flex: 1,
-        flexDirection: (compact ? 'column' : 'row'),
-        alignItems: 'center',
-        backgroundColor: this.props.backgroundColor,
-        justifyContent: 'center' 
-      }}
+    return (
+      <div
+        style={{
+          color: '#607D8B',
+          position: 'relative',
+          display: 'flex',
+          flex: 1,
+          flexDirection: compact ? 'column' : 'row',
+          alignItems: 'center',
+          backgroundColor: this.props.backgroundColor,
+          justifyContent: 'center'
+        }}
       >
-      { blocks.map(b => this.renderBlock(b, index++)) }
-    </div>
+        {blocks.map(b => this.renderBlock(b, index++))}
+      </div>
+    )
   }
 
-  renderDefault (compact) {
-    return this.renderBlocks([
-      this.image(),
-      this.renderContentComponent(compact)
-    ], compact)
+  renderDefault(compact) {
+    return this.renderBlocks(
+      [this.image(), this.renderContentComponent(compact)],
+      compact
+    )
   }
 
-  renderReversed (compact) {
-    return this.renderBlocks([
-      this.renderContentComponent(compact),
-      this.image()
-    ], compact)
+  renderReversed(compact) {
+    return this.renderBlocks(
+      [this.renderContentComponent(compact), this.image()],
+      compact
+    )
   }
 
-  renderContentComponent () {
-    return (this.props.renderContent ? this.props.renderContent() : this.renderContent())
+  renderContentComponent() {
+    return this.props.renderContent
+      ? this.props.renderContent()
+      : this.renderContent()
   }
 
-  renderComponentCompact () {
+  renderComponentCompact() {
     return this.renderDefault(true)
   }
 
-  renderComponent () {
-    return (this.props.reversed ? this.renderReversed() : this.renderDefault())
+  renderComponent() {
+    return this.props.reversed ? this.renderReversed() : this.renderDefault()
   }
 }
--- web/src/components/Navigation.js
+++ web/src/components/Navigation.js
@@ -10,88 +10,147 @@ import { Button } from '@rmwc/button'
 import { Icon } from '@rmwc/icon'
 
 export default class Navigation extends PureComponent {
-
-  constructor (props) {
+  constructor(props) {
     super(props)
     this._onMenuOpen = this.onMenuOpen.bind(this)
-    this._onMenuItem = (item) => this.onMenuItem.bind(this, item)
+    this._onMenuItem = item => this.onMenuItem.bind(this, item)
   }
 
-  onMenuItem (item) {
+  onMenuItem(item) {
     this.props.onMenuItem && this.props.onMenuItem(item)
   }
 
-  renderNavigationMenuItem (item, index) {
-    const MenuIcon = <ToolbarMenuIcon onClick={this._onMenuItem(item)} use={item.icon} style={{
-      color: this.props.theme.navigationTintColor,
-      marginRight: '0px'
-    }} />
-    const MenuButton = <Button onClick={this._onMenuItem(item)}
-      style={{
-        color: this.props.theme.navigationTintColor,
-        textShadow: this.props.theme.textShadow,
-        marginRight: '0px'
-      }}>
-      {item.title}
-    </Button>
-    const MenuActionButton = <Button raised theme='secondary-bg text-primary-on-secondary'
-      onClick={this._onMenuItem(item)}
-      style={{
-        color: this.props.theme.nanvigationTextButton,
-        marginRight: '0px'
-      }}>
-      {`${item.title}`}
-    </Button>
-    return renderResponsive(`menuItem${index++}`, <div />, item.alwaysShowIcon ? MenuIcon : (item.action ? MenuActionButton : MenuButton))
-  }
-
-  onMenuOpen () {
-    this.props.onMenuOpen && this.props.onMenuOpen()
-  }
-
-  renderNavigationMenu () {
-    var index = 0
-    return this.props.menu.map(item => this.renderNavigationMenuItem(item, index++))
-  }
-
-  renderNavigationLogo () {
-    const image = (this.props.navigationUncover ? this.props.theme.logoImage : this.props.theme.logoLightImage)
-    const height = (this.props.navigationUncover ? 64 : 64)
-
-    return renderResponsive('logo',
-      <ToolbarMenuIcon use='menu' style={{ color: this.props.theme.navigationTintColor }} onClick={this._onMenuOpen} />,
-      <img src={`${this.props.desktop ? '../../../../' : '/'}assets/${image}`} style={{ height: `${height}px`, marginLeft: '20px' }} />
+  renderNavigationMenuItem(item, index) {
+    const MenuIcon = (
+      <ToolbarMenuIcon
+        onClick={this._onMenuItem(item)}
+        use={item.icon}
+        style={{
+          color: this.props.theme.navigationTintColor,
+          marginRight: '0px'
+        }}
+      />
+    )
+    const buttonAdditionalStyle = this.props.theme.navigationButtonStyle
+      ? this.props.theme.navigationButtonStyle
+      : {}
+    const MenuButton = (
+      <Button
+        onClick={this._onMenuItem(item)}
+        style={{
+          color: this.props.theme.navigationTintColor,
+          textShadow: this.props.theme.textShadow,
+          marginRight: '0px',
+          ...buttonAdditionalStyle
+        }}
+      >
+        {item.title}
+      </Button>
+    )
+    const actionButtonAdditionalStyle = this.props.theme
+      .navigationActionButtonStyle
+      ? this.props.theme.navigationActionButtonStyle
+      : {}
+    const MenuActionButton = (
+      <Button
+        raised
+        theme="secondary-bg text-primary-on-secondary"
+        onClick={this._onMenuItem(item)}
+        style={{
+          color: this.props.theme.nanvigationTextButton,
+          marginRight: '0px',
+          ...actionButtonAdditionalStyle
+        }}
+      >
+        {`${item.title}`}
+      </Button>
+    )
+    return renderResponsive(
+      `menuItem${index++}`,
+      <div />,
+      item.alwaysShowIcon
+        ? MenuIcon
+        : item.action
+        ? MenuActionButton
+        : MenuButton
     )
   }
 
-  renderDefault () {
-    return (<Toolbar waterfall fixed={this.props.layout.fixed} style={{
-      backgroundColor: this.props.theme.navigationColor
-    }}>
-      <ToolbarRow>
-        <ToolbarSection alignStart style={{
-          flex: 1,
-          display: 'flex',
-          justifyContent: 'left',
-          flexDirection: 'row',
-          alignItems: 'center'
-        }}>
-          {this.renderNavigationLogo()}
-        </ToolbarSection>
-        <ToolbarSection alignEnd style={{
-          flex: 4,
-          display: 'flex',
-          justifyContent: 'flex-end',
-          flexDirection: 'row',
-          alignItems: 'center'
-        }}>
-          {this.renderNavigationMenu()}
-        </ToolbarSection>
-      </ToolbarRow>
-    </Toolbar>)
+  onMenuOpen() {
+    this.props.onMenuOpen && this.props.onMenuOpen()
   }
 
-  render () {
+  renderNavigationMenu() {
+    var index = 0
+    return this.props.menu.map(item =>
+      this.renderNavigationMenuItem(item, index++)
+    )
+  }
+
+  renderNavigationLogo() {
+    const image = this.props.navigationUncover
+      ? this.props.theme.logoImage
+      : this.props.theme.logoLightImage
+    const height = this.props.navigationUncover ? 64 : 64
+
+    return renderResponsive(
+      'logo',
+      <ToolbarMenuIcon
+        use="menu"
+        style={{ color: this.props.theme.navigationTintColor }}
+        onClick={this._onMenuOpen}
+      />,
+      <img
+        src={`${this.props.desktop ? '../../../../' : '/'}assets/${image}`}
+        style={{ height: `${height}px`, marginLeft: '20px' }}
+      />
+    )
+  }
+
+  renderDefault() {
+    const wrapperAdditionalStyle = this.props.theme.navigationWrapperStyle
+      ? this.props.theme.navigationWrapperStyle
+      : {}
+    return (
+      <Toolbar
+        waterfall
+        fixed={this.props.layout.fixed}
+        style={{
+          backgroundColor: this.props.theme.navigationColor,
+          ...wrapperAdditionalStyle
+        }}
+      >
+        <ToolbarRow>
+          <ToolbarSection
+            alignStart
+            style={{
+              flex: 1,
+              display: 'flex',
+              justifyContent: 'left',
+              flexDirection: 'row',
+              alignItems: 'center'
+            }}
+          >
+            {this.renderNavigationLogo()}
+          </ToolbarSection>
+          <ToolbarSection
+            alignEnd
+            style={{
+              flex: 4,
+              display: 'flex',
+              justifyContent: 'flex-end',
+              flexDirection: 'row',
+              alignItems: 'center'
+            }}
+          >
+            {this.renderNavigationMenu()}
+          </ToolbarSection>
+        </ToolbarRow>
+      </Toolbar>
+    )
+  }
+
+  render() {
     return this.renderDefault()
   }
 }
