fa5c583ffc6f34d3257c543e04ffbc29fb08413c
I. Dan Calinescu <idancalinescu@gmail.com>
2020-01-29 02:17:18 +0200

    added cache context tests

--- test/specs/cache.js
+++ test/specs/cache.js
@@ -69,4 +69,20 @@ savor.add('should be able to detect if an item is not cached', (context, done) =
   }, () => {})
 })
 
+.add('should be able handle context caching', (context, done) => {
+  // Inject a mock adapter
+  global.storage = {
+    setItem: (key, value, callback) => callback(),
+    getItem: (key, callback) => callback(null, JSON.stringify({ context: 'context' })),
+    removeItem: (key, callback) => callback()
+  }
+
+  // Make sure the token can be set, retrieved and cached
+  savor.promiseShouldSucceed(Data.Cache.cacheContext({ context: 'context' }), () => {
+    savor.promiseShouldSucceed(Data.Cache.retrieveContext(), () => {
+      savor.promiseShouldSucceed(Data.Cache.clearContext(), done, () => {})
+    }, () => {})
+  }, () => {})
+})
+
 .run('Data Cache')
