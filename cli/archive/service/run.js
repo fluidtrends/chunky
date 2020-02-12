fd6f1666d47c9454307d633b409457db6d6121e1
I. Dan Calinescu <idancalinescu@gmail.com>
2020-02-12 13:02:12 +0200

    cleanedup progressive deps

--- cli/archive/service/run.js
+++
@@ -1,9 +0,0 @@
-// try {
-//     const chunky = JSON.parse(process.env.chunky)
-//     process.chdir(chunky.cwd)
-
-//     const executor = require(`../executors/${chunky.event.executor}`)
-//     executor(Object.assign({}, chunky.event.options, { service: true, env: chunky.event.env }))
-// } catch (e) {
-//     console.log(e)
-// }
--- cli/archive/service/start.js
+++
@@ -1,8 +0,0 @@
-// const Service = require('./Service')
-
-// const service = new Service({ 
-//     port: 13001,
-//     nodeVersion: "8.16.2" 
-// })
-
-// service.start()
