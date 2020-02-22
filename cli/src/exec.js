bc6a8e7b05c72b5754ca7f0ec4866ba0784e0a25
I. Dan Calinescu <idancalinescu@gmail.com>
2020-02-22 09:39:52 +0200

    using carmel plugin

--- cli/src/exec.js
+++ cli/src/exec.js
@@ -1,7 +1,7 @@
 const CarmelPlugin = require('chunky-carmel-plugin')
 
 module.exports = (args) => {
-    const command = Object.assign({}, args, { id: args._[0] })
+    const command = Object.assign({}, { args }, { id: args._[0] })
     const plugin = new CarmelPlugin({ command })
 
     return plugin.run()
