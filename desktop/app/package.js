379f26e120f99fde0472d9b64eecb384eb362ae4
I. Dan Calinescu <idancalinescu@gmail.com>
2020-02-24 09:00:11 +0200

    added desktop package scrip

---
+++ desktop/app/package.js
@@ -0,0 +1,32 @@
+const { api } = require('@electron-forge/core')
+const path = require('path')
+const fs = require('fs-extra')
+
+function buildForPlatform (dir, outDir) {
+  const platform = process.platform
+  const arch = process.arch
+
+  return api.package({
+    dir,
+    prune: false,
+    interactive: true,
+    platform,
+    arch,
+    outDir
+  })
+}
+
+function build (options) {
+  const dir = path.resolve(options.dir)
+  const outDir = path.resolve(path.dirname(options.dir), `chunky-desktop-build`)
+
+  if (fs.existsSync(outDir)) {
+    fs.removeSync(outDir)
+  }
+
+  fs.mkdirsSync(outDir)
+
+  return buildForPlatform(dir, outDir)
+}
+
+module.exports = build
--- desktop/app/start.js
+++ desktop/app/start.js
@@ -3,13 +3,12 @@ const path = require('path')
 
 function start (options) {
   const dir = path.resolve(options.dir)
-  const appPath = path.resolve(options.appPath)
 
   return api.start({
     dir,
     interactive: true,
     enableLogging: true,
-    appPath
+    appPath: dir
   })
 }
 
