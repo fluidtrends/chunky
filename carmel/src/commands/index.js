c34493d1ce0f3de2f3f080fcbbd4574715fba790
I. Dan Calinescu <idancalinescu@gmail.com>
2020-03-24 21:25:49 +0200

    added support for lock unlock and deploy

--- carmel/src/commands/index.js
+++ carmel/src/commands/index.js
@@ -2,10 +2,16 @@ const Init = require('./init')
 const Start = require('./start')
 const Make = require('./make')
 const Preview = require('./preview')
+const Deploy = require('./deploy')
+const Lock = require('./lock')
+const Unlock = require('./unlock')
 
 module.exports = {
     Init,
     Make,
     Preview,
+    Unlock,
+    Lock,
+    Deploy,
     Start
 }
