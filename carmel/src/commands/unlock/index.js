815c69e0d6bfbfebb2282c671545416063cb0ac8
I. Dan Calinescu <idancalinescu@gmail.com>
2020-03-25 19:18:20 +0200

    added full lock and unlock

--- carmel/src/commands/unlock/index.js
+++ carmel/src/commands/unlock/index.js
@@ -1,7 +1,7 @@
 const Carmel = require('@carmel/sdk')
 const coreutils = require('coreutils')
 
-class _ extends Carmel.Commands.Lock {
+class _ extends Carmel.Commands.Unlock {
     constructor(args) {
       super(args)
     }
@@ -11,7 +11,11 @@ class _ extends Carmel.Commands.Lock {
                   .then(() => {
                     coreutils.logger.footer(`Congrats! Your data is now unlocked :)`)
                   })
-   }
+                  .catch((e) => {
+                    console.log(e)
+                    coreutils.logger.error(e)
+                  })
+    }
   }
   
   _.ERRORS = Object.assign({}, _.ERRORS, {})
