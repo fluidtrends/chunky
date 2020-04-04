5133b7668c976fcde721361ec24751de6a02237e
I. Dan Calinescu <idancalinescu@gmail.com>
2020-04-04 23:57:36 +0300

    added cloud command

--- carmel/src/commands/index.js
+++ carmel/src/commands/index.js
@@ -6,10 +6,12 @@ const Deploy = require('./deploy')
 const Data = require('./data')
 const Workspace = require('./workspace')
 const Setup = require('./setup')
+const Cloud = require('./cloud')
 
 module.exports = {
     Init,
     Make,
+    Cloud,
     Preview,
     Workspace,
     Data,
