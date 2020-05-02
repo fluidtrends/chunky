0a3495f0b020e28ff6a35f6959b4ba150edef84b
I. Dan Calinescu <idancalinescu@gmail.com>
2020-05-02 10:36:26 +0300

    added the ability to calculated invested effort

--- src/Release.js
+++ src/Release.js
@@ -46,13 +46,30 @@ class _ {
         return this._flows
     }
 
+    get state() {
+        return this._state
+    }
+
     async load() {
         const milestone = this.milestone.number
-        this._issues = await this.product.services.github.issues({ repo: this.product.repo, milestone })
+        const pageSize = 100
+        const page = Math.ceil((this.milestone.open_issues + this.milestone.closed_issues) / pageSize)
+        this._issues = await this.product.services.github.issues({ repo: this.product.repo, milestone, page, pageSize })
 
-        this._effort = {}
+        const allPullRequests = this.issues.filter(i => i.pull_request)
+        const allIssues = this.issues.filter(i => !i.pull_request)
+        const openPullRequests = this.issues.filter(i => i.pull_request && i.state === "open")
+        const closedPullRequests = this.issues.filter(i => i.pull_request && i.state === "closed")
+        const openIssues = this.issues.filter(i => !i.pull_request && i.state === "open")
+        const closedIssues = this.issues.filter(i => !i.pull_request && i.state === "closed")
+        
+        this._state = {
+            allPullRequests, allIssues, openPullRequests, closedPullRequests, openIssues, closedIssues
+        }
 
-        this.issues.map(issue => {
+        this._effort = {} 
+
+        allIssues.map(issue => {
             const found = issue.title.match(/^([A-Z][0-9]+)\s*:\s*(.*)$/)
             var weight = 0
             issue.labels.map(label => weight = EFFORT_SIZES[label.name] || weight)
@@ -63,9 +80,11 @@ class _ {
 
             const flow = found[1].trim()
 
-            this.effort[flow] = this.effort[flow] || { weight: 0, issues: 0 }
-            this.effort[flow].weight = this.effort[flow].weight + weight
+
+            this.effort[flow] = this.effort[flow] || { required: 0, invested: 0, issues: 0 }
             this.effort[flow].issues = this.effort[flow].issues + 1
+            this.effort[flow].required = this.effort[flow].required + weight
+            this.effort[flow].invested = this.effort[flow].invested + (issue.state === "closed" ? weight : 0)
         })
 
         this._flows = await this.product.dashboard.rows(`${this.name}!A6:ZZ106`)
@@ -78,12 +97,14 @@ class _ {
         var endRow = startRow + this.flows.length
 
         var updates = this.flows.map(([id,,,effort]) => {            
-            const isStep = id.match(/^([A-Z][0-9]+)$/) ? true : false
-            const eff = this.effort[id] ? this.effort[id].weight : (effort || 0) || 0
-            return isStep ? [eff] : []
+            if (!id.match(/^([A-Z][0-9]+)$/) || !this.effort[id]) {
+                return []
+            }
+            
+            return [this.effort[id].required, this.effort[id].invested]
         })
 
-        this.product.dashboard.update(`${this.name}!D${startRow}:D${endRow}`, updates)
+        this.product.dashboard.update(`${this.name}!D${startRow}:E${endRow}`, updates)
     }
 
     generateReport () {
@@ -91,7 +112,13 @@ class _ {
             release: {
                 name: this.name,
                 id: this.id, 
-                effort: this.effort
+                effort: this.effort,
+                allPullRequests: this.state.allPullRequests.length,
+                allIssues: this.state.allIssues.length,
+                openPullRequests: this.state.openPullRequests.length,
+                closedPullRequests: this.state.closedPullRequests.length,
+                openIssues: this.state.openIssues.length,
+                closedIssues: this.state.closedIssues.length
             }
         })
     }
--- src/services/github/index.js
+++ src/services/github/index.js
@@ -2,14 +2,43 @@ const { Octokit } = require("@octokit/rest")
 const moment = require('moment')
 
 const MILESTONE_FIELDS = ["number", "title", "id", "open_issues", "closed_issues", "state", "due_on", "closed_at", "description"]
-const ISSUE_FIELDS = ["number", "title", "id", "open_issues", "labels", "body", "state", "due_on", "closed_at"]
-const IDS = { milestone: "number", assignee: "login" }
+const ISSUE_FIELDS = ["number", "title", "id", "open_issues", "labels", "body", "state", "due_on", "closed_at", "pull_request"]
+const PULL_FIELDS = [["head", ["label", "ref", "sha", ["repo", ["full_name"]], ["user", ["login"]]]], ["user", ["login"]], ["milestone", ["number"]], "labels", "created_at", "updated_at", "closed_at", "body", "merged_at", "merge_commit_sha", "title", "state", "number", "id"]
+const EVENT_FIELDS = ["id", ["milestone", ["number"] ], ["issue", ["number", "state", "labels", ["milestone", ["number"]]]], ["actor", ["login"]], "event", "commit_id", "created_at"]
 
-const _cleanFields = (data, fields) => data.map(item => Object.keys(item).map(k => !fields.includes(k) && delete item[k]) && item)
+const _clean = (item, fields) => {
+    var result = {}
+
+    fields.map(field => {
+        if (typeof field !== "string" && item[field[0]]) {
+            result[field[0]] = _clean(item[field[0]], field[1])
+            return
+        } 
+
+        if (!item[field]) {
+            return
+        }
+
+        result[field] = item[field]
+    })
+
+    return result
+}
+
 const milestones = async (repo) => _._.issues.listMilestonesForRepo({ owner: repo.split('/')[0], repo: repo.split('/')[1] })
-                                              .then(({ data }) => _cleanFields(data, MILESTONE_FIELDS))
-const issues = async ({ repo, milestone }) => _._.issues.listForRepo({ owner: repo.split('/')[0], repo: repo.split('/')[1], milestone })
-                                              .then(({ data }) => _cleanFields(data, ISSUE_FIELDS))
+                                              .then(({ data }) => data.map(item => _clean(item, MILESTONE_FIELDS)))
+
+const issues = async ({ repo, milestone, pageSize, page, buffer }) => (!page || page === 0) ? (buffer || []) : 
+        _._.issues.listForRepo({ owner: repo.split('/')[0], repo: repo.split('/')[1], milestone, state: "all", per_page: (pageSize || 100), page })
+                .then(({ data }) => data.map(item => _clean(item, ISSUE_FIELDS)))
+                .then((data) => issues({ repo, milestone, pageSize, page: (page-1), buffer: (buffer || []).concat(data) }))
+
+const pulls = async ({ repo, milestone }) => _._.pulls.list({ owner: repo.split('/')[0], repo: repo.split('/')[1], state: "all", sort: "created", direction: "desc", per_page: 100 })
+                                                      .then(({ data }) => data.filter(d => d.milestone && `${d.milestone.number}` === `${milestone}`))
+                                                      .then((data) => data.map(item => _clean(item, PULL_FIELDS)))
+const events = async ({ repo, milestone }) => _._.issues.listEventsForRepo({ owner: repo.split('/')[0], repo: repo.split('/')[1], per_page: 100 })
+                                                      .then(({ data }) => data)
+                                                      .then((data) => data.map(item => _clean(item, EVENT_FIELDS)))
 
 const init = async () => new Promise((resolve, reject) => {
     if (!process.env.CHUNKY_GITHUB_PERSONAL_TOKEN) {
@@ -21,7 +50,7 @@ const init = async () => new Promise((resolve, reject) => {
 })
 
 const _ = {
-    init, milestones, issues
+    init, milestones, issues, pulls, events
 }
 
 module.exports = _
